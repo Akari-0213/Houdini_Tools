f4cc7fcb62a3d8128758dfc3c50f37fa6409a74e
Akari-0213 <anishioka.cherry@icloud.com>
2026-08-10 20:31:04 +0900

    seed欄削除、Rotate枠線追加、クリア、フォルダ選択機能追加

--- fbx_importer/fbx_importer_of_shlef_layout.py
+++ fbx_importer/fbx_importer_of_shlef_layout.py
@@ -7,6 +7,11 @@ SETTINGS_PATH= pathlib.Path(r"C:\\Houdini\\houdini_scripts\\fbx_importer\\fbximp
 
 
 def delete_item(fbx_list :any) -> None:
+    selected_node = hou.selectedNodes()[0].path()
+    if not selected_node:
+        print("shelf_dopノードが選択されていません")
+        return
+
     current_item = fbx_list.currentItem()
     if not current_item:
         print("削除するアイテムが選択されていません")
@@ -14,7 +19,7 @@ def delete_item(fbx_list :any) -> None:
     full_path = current_item.data(QtCore.Qt.UserRole)
     
     file_name = current_item.text()
-    target_node_path = f"/obj/shelf_dop/{file_name}"
+    target_node_path = f"{selected_node}/{file_name}"
     target_node = hou.node(target_node_path)
     if target_node:
         target_node.destroy()
@@ -36,24 +41,28 @@ def delete_item(fbx_list :any) -> None:
     row = fbx_list.row(current_item)
     fbx_list.takeItem(row)
 
-    assign_id_wrangle_node = hou.node("/obj/shelf_dop/attribwrangle_assign_id")
+    assign_id_wrangle_node = hou.node(f"{selected_node}/attribwrangle_assign_id")
     if assign_id_wrangle_node:
         all_weights = create_weight_str()
         set_weight_to_wrangle(assign_id_wrangle_node, all_weights)
 
 
-def update_node(file_path: str, scale_param: float, rotation: tuple[float, float, float], maxstack_num: int, stack_seed: int, weight: int) -> None:
+def update_node(file_path: str, scale_param: float, rotation: tuple[float, float, float], maxstack_num: int, weight: int) -> None:
+    selected_node = hou.selectedNodes()[0].path()
+    if not selected_node:
+        print("shelf_dopノードが選択されていません")
+        return
     file_name: str = pathlib.Path(file_path).name
     settings = load_setting()
     if file_path not in settings:
         print(f"{file_path}は追加されていません")
         return
-    save_json(file_path, scale_param, rotation, maxstack_num, stack_seed, weight)
+    save_json(file_path, scale_param, rotation, maxstack_num, weight)
     all_weights = create_weight_str()
-    file_node = hou.node(f"/obj/shelf_dop/{file_name}/file1")
-    trans_node = hou.node(f"/obj/shelf_dop/{file_name}/transform1")
-    wrangle_node = hou.node(f"/obj/shelf_dop/{file_name}/attribwrangle1")
-    assign_id_wrangle_node = hou.node("/obj/shelf_dop/attribwrangle_assign_id")
+    file_node = hou.node(f"{selected_node}/{file_name}/file1")
+    trans_node = hou.node(f"{selected_node}/{file_name}/transform1")
+    wrangle_node = hou.node(f"{selected_node}/{file_name}/attribwrangle1")
+    assign_id_wrangle_node = hou.node(f"{selected_node}/attribwrangle_assign_id")
     
     if file_path.startswith("file:///"):
         new_file_path = re.split("file:///", file_path)[1]
@@ -68,26 +77,29 @@ def update_node(file_path: str, scale_param: float, rotation: tuple[float, float
     trans_node.parm("rz").set(rz)
     
     wrangle_node.parm("snippet").set(
-        f"""int seed = {stack_seed};
-        int maxstack_num = {maxstack_num} + 1;
-        i@stack_num = int(fit(rand(seed),0,1,1,maxstack_num
-        ));""")
+        f"""
+        i@stack_num = {maxstack_num};
+        """)
     
     set_weight_to_wrangle(assign_id_wrangle_node, all_weights)
 
 
-def create_file_node(fbx_list: any, file_path: str, scale_param: float, rotation: tuple[float, float, float], maxstack_num: int, stack_seed: int, weight: int) -> None:
+def create_file_node(fbx_list: any, file_path: str, scale_param: float, rotation: tuple[float, float, float], maxstack_num: int, weight: int) -> None:
+    shelf_geometry_node = hou.selectedNodes()[0]
+    if not shelf_geometry_node:
+        print("shelf_dopノードが選択されていません")
+        return
+
     file_name: str = pathlib.Path(file_path).name
     settings = load_setting()
     if file_path in settings:
         print(f"すでに{file_path}は存在しています")
         return
     
-    save_json(file_path, scale_param, rotation, maxstack_num, stack_seed, weight)
+    save_json(file_path, scale_param, rotation, maxstack_num, weight)
     all_weights = create_weight_str()
     set_list(fbx_list, file_name, file_path)
 
-    shelf_geometry_node = hou.node("/obj/shelf_dop")
     subnet_node = shelf_geometry_node.createNode("subnet", f"{file_name}")
     new_file_node = subnet_node.createNode("file")
     trans_node = subnet_node.createNode("xform")
@@ -95,7 +107,7 @@ def create_file_node(fbx_list: any, file_path: str, scale_param: float, rotation
     pack_node = subnet_node.createNode("pack")
     wrangle_node = subnet_node.createNode("attribwrangle")
     output_node = subnet_node.createNode("output")
-    assign_id_wrangle_node = hou.node("/obj/shelf_dop/attribwrangle_assign_id")
+    assign_id_wrangle_node = hou.node(f"{shelf_geometry_node.path()}/attribwrangle_assign_id")
 
     trans_node.setInput(0, new_file_node)
     matchsize_node.setInput(0, trans_node)
@@ -118,15 +130,14 @@ def create_file_node(fbx_list: any, file_path: str, scale_param: float, rotation
     matchsize_node.parm("justify_y").set("min")
     wrangle_node.parm("class").set(1)
     wrangle_node.parm("snippet").set(
-        f"""int seed = {stack_seed};
-        int maxstack_num = {maxstack_num} + 1;
-        i@stack_num = int(fit(rand(seed),0,1,1,maxstack_num
-        ));""")
+        f"""
+        i@stack_num = {maxstack_num};
+        """)
         
     set_weight_to_wrangle(assign_id_wrangle_node, all_weights)
     
     print(f"ファイルが追加されました{file_name}")
-    merge_node = hou.node("/obj/shelf_dop/merge_fbx")
+    merge_node = hou.node(f"{shelf_geometry_node.path()}/merge_fbx")
     new_file_node_inputtednum = len(merge_node.inputs())
     merge_node.setInput(new_file_node_inputtednum, subnet_node)
 
@@ -143,7 +154,7 @@ foreach(float w; weights){{
 
 
 //重み間での自動生成
-float r = rand(@class + chi("seed")) * total_w;
+float r = rand(@ptnum + chi("seed")) * total_w;
 float num = 0;
 i@fbx_id = 0;
 // 累積判定のループ
@@ -157,7 +168,7 @@ for(int i = 0; i < len(weights); i++){{
 """)
 
 
-def save_json(file_path: pathlib.Path, scale_param: float, rotation: tuple[float, float, float], maxstack_num: int, stack_seed: int, weight: int) -> None:
+def save_json(file_path: pathlib.Path, scale_param: float, rotation: tuple[float, float, float], maxstack_num: int, weight: int) -> None:
     settings = load_setting()
     if file_path not in settings:
         settings[file_path] = {}
@@ -168,7 +179,6 @@ def save_json(file_path: pathlib.Path, scale_param: float, rotation: tuple[float
     settings[file_path]["ry"] = ry
     settings[file_path]["rz"] = rz
     settings[file_path]["maxstack_num"] = maxstack_num
-    settings[file_path]["stack_seed"] = stack_seed
     settings[file_path]["weight"] = weight
     
     try:
@@ -217,7 +227,6 @@ def on_selected_Item(fbx_list: QtWidgets.QListWidget, path_line: QtWidgets.QLine
                      rotate_y_spinbox: QtWidgets.QDoubleSpinBox, 
                      rotate_z_spinbox: QtWidgets.QDoubleSpinBox, 
                      stacknum_spinbox: QtWidgets.QDoubleSpinBox,
-                     seed_spinbox: QtWidgets.QDoubleSpinBox,
                      weight_spinbox: QtWidgets.QDoubleSpinBox):
     settings = load_setting()
     current_item = fbx_list.currentItem()
@@ -234,7 +243,6 @@ def on_selected_Item(fbx_list: QtWidgets.QListWidget, path_line: QtWidgets.QLine
     item_ry = current_item_setting.get('ry', 0.0)
     item_rz = current_item_setting.get('rz', 0.0)
     item_stacknum = current_item_setting.get('maxstack_num', 1)
-    item_stackseed = current_item_setting.get('stack_seed', 1)
     item_weight = current_item_setting.get('weight', 1)
     
     scale_spinbox.setValue(item_scale)
@@ -242,7 +250,6 @@ def on_selected_Item(fbx_list: QtWidgets.QListWidget, path_line: QtWidgets.QLine
     rotate_y_spinbox.setValue(item_ry)
     rotate_z_spinbox.setValue(item_rz)
     stacknum_spinbox.setValue(item_stacknum)
-    seed_spinbox.setValue(item_stackseed)
     weight_spinbox.setValue(item_weight)
 
 class mainWindow(QtWidgets.QWidget):
@@ -251,10 +258,21 @@ class mainWindow(QtWidgets.QWidget):
         self.fbx_path_line = QtWidgets.QLineEdit()
         self.scale_spinbox = settings_spinbox(0.0, 5.0, 3, 0.01)
         self.rotate_x_spinbox = settings_spinbox(0.0, 360, 1, 1.0)
+        x_css = '''
+        border: 2px solid hsv(0, 90, 100);
+        '''
+        self.rotate_x_spinbox.setStyleSheet(x_css)
         self.rotate_y_spinbox = settings_spinbox(0.0, 360, 1, 1.0)
+        y_css = '''
+        border: 2px solid hsv(120, 90, 80);
+        '''
+        self.rotate_y_spinbox.setStyleSheet(y_css)
         self.rotate_z_spinbox = settings_spinbox(0.0, 360, 1, 1.0)
+        z_css = '''
+        border: 2px solid hsv(220, 90, 100);
+        '''
+        self.rotate_z_spinbox.setStyleSheet(z_css)
         self.stacknum_spinbox = settings_spinbox(1.0, 10, 0, 1)
-        self.seed_spinbox = settings_spinbox(1.0, 100, 0, 1)
         self.weight_spinbox = settings_spinbox(1.0, 100, 0, 1)
         self.fbx_list = QtWidgets.QListWidget()
 
@@ -262,8 +280,8 @@ class mainWindow(QtWidgets.QWidget):
         list_layout = self.set_list_layout()
 
         layout = QtWidgets.QHBoxLayout()
-        layout.addLayout(import_layout)
-        layout.addLayout(list_layout)
+        layout.addLayout(import_layout, 3)
+        layout.addLayout(list_layout, 1)
         self.setLayout(layout)
     
     def get_rotation(self) -> tuple[float, float, float]:
@@ -277,9 +295,15 @@ class mainWindow(QtWidgets.QWidget):
         import_layout = QtWidgets.QVBoxLayout()
         fbx_layout = QtWidgets.QHBoxLayout()
         fbx_label = QtWidgets.QLabel("fbx_filepath")
+        file_clear_button = QtWidgets.QPushButton("クリア")
         select_file_button = QtWidgets.QPushButton("ファイル選択")
+        file_clear_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        select_file_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        file_clear_button.clicked.connect(self.clicked_clear)
+        select_file_button.clicked.connect(self.select_file)
         fbx_layout.addWidget(fbx_label)
         fbx_layout.addWidget(self.fbx_path_line)
+        fbx_layout.addWidget(file_clear_button)
         fbx_layout.addWidget(select_file_button)
         import_layout.addLayout(fbx_layout)
 
@@ -295,9 +319,6 @@ class mainWindow(QtWidgets.QWidget):
         
         param_layout.addWidget(QtWidgets.QLabel("MaxStackNum"), 2, 0)
         param_layout.addWidget(self.stacknum_spinbox, 2, 1)
-        
-        param_layout.addWidget(QtWidgets.QLabel("Stack_Seed"), 3, 0)
-        param_layout.addWidget(self.seed_spinbox, 3, 1)
         
         param_layout.addWidget(QtWidgets.QLabel("Appearance_Weight"), 4, 0)
         param_layout.addWidget(self.weight_spinbox, 4, 1)
@@ -324,7 +345,7 @@ class mainWindow(QtWidgets.QWidget):
             self.fbx_list, self.fbx_path_line, 
             self.scale_spinbox, 
             self.rotate_x_spinbox, self.rotate_y_spinbox, self.rotate_z_spinbox,
-            self.stacknum_spinbox, self.seed_spinbox, self.weight_spinbox))
+            self.stacknum_spinbox, self.weight_spinbox))
             
         list_layout.addWidget(self.fbx_list)
         delete_button = QtWidgets.QPushButton("アイテム削除")
@@ -339,8 +360,7 @@ class mainWindow(QtWidgets.QWidget):
             self.fbx_list, self.fbx_path_line.text(), 
             self.scale_spinbox.value(),
             self.get_rotation(), 
-            int(self.stacknum_spinbox.value()), 
-            int(self.seed_spinbox.value()), 
+            int(self.stacknum_spinbox.value()),
             int(self.weight_spinbox.value())))
         button_layout.addWidget(add_button)
         
@@ -350,11 +370,17 @@ class mainWindow(QtWidgets.QWidget):
             self.scale_spinbox.value(), 
             self.get_rotation(),
             int(self.stacknum_spinbox.value()), 
-            int(self.seed_spinbox.value()), 
             int(self.weight_spinbox.value())))
         button_layout.addWidget(update_button)
         return button_layout
 
+    def clicked_clear(self):
+        self.fbx_path_line.clear()
+
+    def select_file(self):
+        selected_file, _ = QtWidgets.QFileDialog.getOpenFileName(self, "ファイルを選択してください。")
+        if selected_file:
+            self.fbx_path_line.setText(selected_file)
 
 dialog = mainWindow()
 dialog.setGeometry(150, 150, 500, 300)
